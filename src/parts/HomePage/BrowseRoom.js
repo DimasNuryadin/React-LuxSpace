f72a5bc3898fcc3d8757c6e498ff17f2c935ff2c
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2024-12-08 19:36:10 +0700

    create useSafeDispatch

--- src/parts/HomePage/BrowseRoom.js
+++ src/parts/HomePage/BrowseRoom.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import useAsync from 'helpers/hooks/useAsync'
 
 export default function BrowseRoom() {
+  const { data, status, error, run, isLoading } = useAsync({ data: { username: "" } })
+  useEffect(() => {
+    run(fetch("https://523e51a0-7121-4b16-9e83-065bc67bbb89.mock.pstmn.io/api/categories/?page=1&limit=4"))
+      .then(async response => {
+        const jsonResponse = await response.json()
+        // console.log(jsonResponse)
+        if (response.ok) return jsonResponse
+
+        throw new Error(JSON.stringify(jsonResponse))
+      })
+  }, [run])
+
+  console.log(data, status, error);
+
+  if (isLoading) return "Loading";
+
   return (
     <section className="flex bg-gray-100 py-16 px-4" id="browse-the-room">
       <div className="container mx-auto">
