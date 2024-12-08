39defc5653f1fdd52d5434c7b0b12a5e72e0acac
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2024-12-08 12:54:48 +0700

    create hooks scrollAnchor and modalDOM

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import Header from 'parts/Header'
-import Hero from 'parts/Hero'
+import Hero from 'parts/HomePage/Hero'
 import BrowseRoom from '../parts/HomePage/BrowseRoom'
 import JustArrive from 'parts/HomePage/JustArrive'
 import React from 'react'
@@ -7,7 +7,13 @@ import Clients from 'parts/Clients'
 import Sitemap from 'parts/Sitemap'
 import Footer from 'parts/Footer'
 
+import useScrollAnchor from 'helpers/hooks/useScrollAnchor'
+import useModalDOM from 'helpers/hooks/useModalDOM'
+
 export default function HomePage(props) {
+  useScrollAnchor();
+  useModalDOM();
+
   return (
     <>
       <Header theme="white" position="absolute" />
